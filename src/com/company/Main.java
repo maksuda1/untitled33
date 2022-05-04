4f8509d8a9f14d81526e0ed24d08c6c877dc01b4
maksudahan <maksuda.2289@gmail.com>
2022-05-04 12:23:06 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+              Triangle triangle = new Triangle();
+                triangle.a = 4;
+                triangle.b = 5;
+                triangle.c = 6;
+
+                triangle.area();
+
+
+    }}
+
+
+
+
+
+
+
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Triangle {
+        double a;
+        double b;
+        double c;
+
+        void area() {
+            double p = (a + b + c) / 2;
+            System.out.println(Math.sqrt(p = (p - a) * (p - b) * (p - c)));
+
+
+        }}
+
+
+
